7c4fd93a5e69d98dff181f19a349e722a62ba960
Goldenfreddy0703 <jenkins30661@gmail.com>
2022-06-17 17:12:56 -0400

    Rawr

--- repo/context.kaito/findSimilar.py
+++ repo/context.kaito/findSimilar.py
@@ -1,4 +1,6 @@
 import sys, xbmc
+import urllib.parse
+import json
 
 if __name__ == '__main__':
 
@@ -7,6 +9,13 @@ if __name__ == '__main__':
     path = item.getPath()
     plugin = 'plugin://plugin.video.kaito/'
     path = path.split(plugin, 1)[1]
+    params = urllib.parse.parse_qs(path.lstrip('?/'))
+    action_args = params['action_args'][0]
+    action_args = json.loads(urllib.parse.unquote(action_args))
+    anilist_id = action_args['anilist_id']
+    action_path = "plugin://plugin.video.kaito?action=find_similar&action_args=%s" % params['action_args'][0]
+    import web_pdb
+    web_pdb.set_trace()
 
-    xbmc.executebuiltin('ActivateWindow(Videos,plugin://plugin.video.kaito/find_similar/%s)'
-                        % path)
+    xbmc.executebuiltin('ActivateWindow(Videos,%s,return)'
+                        % action_path)

ace98f1c4e62100eebfeddfe0f3414475ba14152
Goldenfreddy0703 <jenkins30661@gmail.com>
2022-09-08 04:26:57 +0100

    Rawr

--- repo/plugin.video.seren/resources/lib/third_party/tzlocal/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import _winreg as winreg
-    from .win32 import get_localzone, reload_localzone
-except ImportError:
-    try:
-        import winreg
-        from .win32 import get_localzone, reload_localzone
-    except ImportError:
-        from .unix import get_localzone, reload_localzone
--- repo/script.skinvariables/script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Module: default
-# Author: jurialmunkey
-# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-from resources.lib.script import Script
-
-if __name__ == '__main__':
-    Script().run()

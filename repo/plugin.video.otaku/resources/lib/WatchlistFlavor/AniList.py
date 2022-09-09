968bb89727361e92c4bc7e8adf6461ff33341363
Goldenfreddy0703 <jenkins30661@gmail.com>
2022-09-08 23:13:40 -0400

    Rawr

---
+++ repo/plugin.video.otaku/resources/lib/WatchlistFlavor/AniList.py
@@ -0,0 +1,437 @@
+import itertools
+from resources.lib.ui import database
+from resources.lib.WatchlistFlavor.WatchlistFlavorBase import WatchlistFlavorBase
+
+
+class AniListWLF(WatchlistFlavorBase):
+    _URL = "https://graphql.anilist.co"
+    _TITLE = "AniList"
+    _NAME = "anilist"
+    _IMAGE = "anilist.png"
+
+    # Not login, but retrieveing userId for watchlist
+    def login(self):
+        query = '''
+        query ($name: String) {
+            User(name: $name) {
+                id
+                }
+            }
+        '''
+
+        variables = {
+            "name": self._username
+        }
+
+        result = self._post_request(self._URL, json={'query': query, 'variables': variables})
+        results = result.json()
+
+        if "errors" in results.keys():
+            return
+
+        userId = results['data']['User']['id']
+
+        login_data = {
+            'userid': str(userId)
+        }
+
+        return login_data
+
+    def watchlist(self):
+        return self._process_watchlist_view("watchlist/%d", page=1)
+
+    def _base_watchlist_view(self, res):
+        base = {
+            "name": res[0],
+            "url": 'watchlist_status_type/%s/%s' % (self._NAME, res[1]),
+            "image": res[0].lower() + '.png',
+            "plot": '',
+        }
+
+        return self._parse_view(base)
+
+    def _process_watchlist_view(self, base_plugin_url, page):
+        all_results = list(map(self._base_watchlist_view, self.__anilist_statuses()))
+        all_results = list(itertools.chain(*all_results))
+        return all_results
+
+    def __anilist_statuses(self):
+        statuses = [
+            ("Next Up", "CURRENT?next_up=true"),
+            ("Current", "CURRENT"),
+            ("Rewatching", "REPEATING"),
+            ("Plan to Watch", "PLANNING"),
+            ("Paused", "PAUSED"),
+            ("Completed", "COMPLETED"),
+            ("Dropped", "DROPPED"),
+        ]
+
+        return statuses
+
+    def get_watchlist_status(self, status, next_up):
+        query = '''
+        query ($userId: Int, $userName: String, $status: MediaListStatus, $type: MediaType, $sort: [MediaListSort]) {
+            MediaListCollection(userId: $userId, userName: $userName, status: $status, type: $type, sort: $sort) {
+                lists {
+                    entries {
+                        ...mediaListEntry
+                        }
+                    }
+                }
+            }
+
+        fragment mediaListEntry on MediaList {
+            id
+            mediaId
+            status
+            progress
+            customLists
+            media {
+                id
+                idMal
+                title {
+                    userPreferred,
+                    romaji,
+                    english
+                }
+                coverImage {
+                    extraLarge
+                }
+                startDate {
+                    year,
+                    month,
+                    day
+                }
+                nextAiringEpisode {
+                    episode,
+                    airingAt
+                }
+                description
+                synonyms
+                format
+                status
+                episodes
+                genres
+                duration
+                countryOfOrigin
+                averageScore
+                characters (
+                    page: 1,
+                    sort: ROLE,
+                    perPage: 10,
+                ) {
+                    edges {
+                        node {
+                            name {
+                                userPreferred
+                            }
+                        }
+                        voiceActors (language: JAPANESE) {
+                            name {
+                                userPreferred
+                            }
+                            image {
+                                large
+                            }
+                        }
+                    }
+                }
+                studios {
+                    edges {
+                        node {
+                            name
+                        }
+                    }
+                }
+                trailer {
+                    id
+                    site
+                }
+            }
+        }
+        '''
+
+        variables = {
+            'userId': int(self._user_id),
+            'username': self._username,
+            'status': status,
+            'type': 'ANIME',
+            'sort': [self.__get_sort()]
+        }
+
+        return self._process_status_view(query, variables, next_up, "watchlist/%d", page=1)
+
+    def get_watchlist_anime_entry(self, anilist_id):
+        query = '''
+        query ($mediaId: Int) {
+            Media (id: $mediaId) {
+                id
+                mediaListEntry {
+                    id
+                    mediaId
+                    status
+                    score
+                    progress
+                    user {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+        '''
+
+        variables = {
+            'mediaId': anilist_id
+        }
+
+        result = self._post_request(self._URL, headers=self.__headers(), json={'query': query, 'variables': variables})
+        results = result.json()['data']['Media']['mediaListEntry']
+
+        anime_entry = {}
+        anime_entry['eps_watched'] = results['progress']
+        anime_entry['status'] = results['status'].title()
+        anime_entry['score'] = results['score']
+
+        return anime_entry
+
+    def _process_status_view(self, query, variables, next_up, base_plugin_url, page):
+        result = self._post_request(self._URL, json={'query': query, 'variables': variables})
+        results = result.json()
+
+        if "errors" in results.keys():
+            return
+
+        try:
+            entries = results['data']['MediaListCollection']['lists'][0]['entries']
+        except IndexError:
+            entries = []
+
+        if next_up:
+            all_results = list(map(self._base_next_up_view, reversed(entries)))
+        else:
+            all_results = list(map(self._base_watchlist_status_view, reversed(entries)))
+
+        all_results = [i for i in all_results if i is not None]
+
+        all_results = list(itertools.chain(*all_results))
+        return all_results
+
+    def _base_watchlist_status_view(self, res):
+        progress = res['progress']
+        res = res['media']
+
+        # remove cached eps for releasing shows every five days so new eps metadata can be shown
+        if res.get('status') == 'RELEASING':
+            try:
+                from datetime import datetime, timedelta
+                check_update = (datetime.today() - timedelta(days=5)).strftime('%Y-%m-%d')
+                last_updated = database.get_episode_list(116006)[0]['last_updated']
+                if check_update == last_updated:
+                    database.remove_episodes(res['id'])
+            except:
+                pass
+
+        # kodi_meta = self._get_kodi_meta(res['id'], 'anilist')
+
+        info = {}
+
+        info['genre'] = res.get('genres')
+
+        desc = res.get('description')
+        if desc:
+            desc = desc.replace('<i>', '[I]').replace('</i>', '[/I]')
+            desc = desc.replace('<b>', '[B]').replace('</b>', '[/B]')
+            desc = desc.replace('<br>', '[CR]')
+            desc = desc.replace('\n', '')
+            info['plot'] = desc
+
+        try:
+            info['title'] = res['title']['userPreferred']
+        except:
+            pass
+
+        try:
+            info['duration'] = res.get('duration') * 60
+        except:
+            pass
+
+        try:
+            start_date = res.get('startDate')
+            info['aired'] = '{}-{:02}-{:02}'.format(start_date['year'], start_date['month'], start_date['day'])
+        except:
+            pass
+
+        try:
+            info['status'] = res.get('status')
+        except:
+            pass
+
+        info['mediatype'] = 'tvshow'
+
+        info['country'] = res.get('countryOfOrigin', '')
+
+        try:
+            cast = []
+            cast2 = []
+            for x in res.get('characters').get('edges'):
+                role = x.get('node').get('name').get('userPreferred')
+                actor = x.get('voiceActors')[0].get('name').get('userPreferred')
+                actor_hs = x.get('voiceActors')[0].get('image').get('large')
+                cast.append((actor, role))
+                cast2.append({'name': actor, 'role': role, 'thumbnail': actor_hs})
+            info['cast'] = cast
+            info['cast2'] = cast2
+        except:
+            pass
+
+        try:
+            info['studio'] = [x.get('node').get('name') for x in res.get('studios').get('edges')]
+        except:
+            pass
+
+        try:
+            info['rating'] = res.get('averageScore') / 10.0
+        except:
+            pass
+
+        try:
+            if res.get('trailer').get('site') == 'youtube':
+                info['trailer'] = 'plugin://plugin.video.youtube/play/?video_id={0}'.format(res.get('trailer').get('id'))
+            else:
+                info['trailer'] = 'plugin://plugin.video.dailymotion_com/?url={0}&mode=playVideo'.format(res.get('trailer').get('id'))
+        except:
+            pass
+
+        base = {
+            "name": '%s - %d/%d' % (res["title"]["userPreferred"], progress, res['episodes'] if res['episodes'] is not None else 0),
+            "url": "watchlist_query/%s/%s/%d" % (res['id'], res.get('idMal'), progress),
+            "image": res['coverImage']['extraLarge'],
+            "plot": info,
+        }
+
+        if res['format'] == 'MOVIE' and res['episodes'] == 1:
+            base['url'] = "watchlist_to_movie/?anilist_id=%s" % (res['id'])
+            base['plot']['mediatype'] = 'movie'
+            return self._parse_view(base, False)
+
+        return self._parse_view(base)
+
+    def _base_next_up_view(self, res):
+        progress = res['progress']
+        res = res['media']
+        next_up = progress + 1
+        episode_count = res['episodes'] if res['episodes'] is not None else 0
+        title = '%s - %s/%s' % (res['title']['userPreferred'], next_up, episode_count)
+        poster = image = res['coverImage']['extraLarge']
+        plot = None
+
+        if episode_count > 0 and next_up > episode_count:
+            return None
+
+        if res['nextAiringEpisode'] is not None and next_up == res['nextAiringEpisode']['episode']:
+            return None
+
+        anilist_id, next_up_meta = self._get_next_up_meta('', progress, res['id'])
+        if next_up_meta:
+            url = 'play/%d/%d/' % (anilist_id, next_up)
+            if next_up_meta.get('title'):
+                title = '%s - %s' % (title, next_up_meta.get('title'))
+            if next_up_meta.get('image'):
+                image = next_up_meta.get('image')
+            plot = next_up_meta.get('plot')
+
+        info = {}
+
+        try:
+            info['genre'] = res.get('genres')
+        except:
+            pass
+
+        info['episode'] = next_up
+
+        info['title'] = title
+
+        info['tvshowtitle'] = res['title']['userPreferred']
+
+        info['plot'] = plot
+
+        info['mediatype'] = 'episode'
+
+        base = {
+            "name": title,
+            "url": "watchlist_query/%s/%s/%d" % (res['id'], res.get('idMal'), progress),
+            "image": image,
+            "plot": info,
+            "fanart": image,
+            "poster": poster,
+        }
+
+        if next_up_meta:
+            base['url'] = url
+            return self._parse_view(base, False)
+
+        if res['format'] == 'MOVIE' and res['episodes'] == 1:
+            base['url'] = "watchlist_to_movie/?anilist_id=%s" % (res['id'])
+            base['plot']['mediatype'] = 'movie'
+            return self._parse_view(base, False)
+
+        return self._parse_view(base)
+
+    def _get_titles(self, res):
+        titles = list(set(res['title'].values())) + res.get('synonyms', [])[:2]
+        if res['format'] == 'MOVIE':
+            titles = list(res['title'].values())
+        # titles = filter(lambda x: all(ord(char) < 128 for char in x) if x else [], titles)
+        titles = [x for x in titles if (all(ord(char) < 128 for char in x) if x else [])]
+        titles = '|'.join(titles[:3])
+        return titles
+
+    def __get_sort(self):
+        sort_types = {
+            "Score": "SCORE",
+            "Progress": "PROGRESS",
+            "Last Updated": "UPDATED_TIME",
+            "Last Added": "ADDED_TIME",
+        }
+
+        return sort_types[self._sort]
+
+    def __headers(self):
+        headers = {
+            'Authorization': 'Bearer ' + self._token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+
+        return headers
+
+    def _kitsu_to_anilist_id(self, kitsu_id):
+        arm_resp = self._get_request("https://arm.now.sh/api/v1/search?type=kitsu&id=" + kitsu_id)
+        if arm_resp.status_code != 200:
+            raise Exception("AnimeID not found")
+
+        anilist_id = arm_resp.json()["services"]["anilist"]
+        return anilist_id
+
+    def watchlist_update(self, anilist_id, episode):
+        return lambda: self.__update_library(episode, anilist_id)
+
+    def __update_library(self, episode, anilist_id):
+        query = '''
+        mutation ($mediaId: Int, $progress : Int, $status: MediaListStatus) {
+            SaveMediaListEntry (mediaId: $mediaId, progress: $progress, status: $status) {
+                id
+                progress
+                status
+                }
+            }
+        '''
+
+        variables = {
+            'mediaId': int(anilist_id),
+            'progress': int(episode),
+            'status': 'CURRENT'
+        }
+
+        self._post_request(self._URL, headers=self.__headers(), json={'query': query, 'variables': variables})
---
+++ repo/plugin.video.otaku/resources/lib/WatchlistIntegration.py
@@ -0,0 +1,160 @@
+import ast
+from resources.lib.ui import control
+from resources.lib.ui.router import route
+from resources.lib.WatchlistFlavor import WatchlistFlavor
+from resources.lib.ui import database
+
+_BROWSER = None
+
+
+def set_browser(browser):
+    global _BROWSER
+    _BROWSER = browser
+
+
+def get_anilist_res(mal_id):
+    title_lang = control.getSetting("titlelanguage")
+    from resources.lib.AniListBrowser import AniListBrowser
+    return AniListBrowser(title_lang).get_mal_to_anilist(mal_id)
+
+
+def get_auth_dialog(flavor):
+    import sys
+    from resources.lib.windows import wlf_auth
+
+    platform = sys.platform
+
+    if 'linux' in platform:
+        auth = wlf_auth.AltWatchlistFlavorAuth(flavor).set_settings()
+    else:
+        auth = wlf_auth.WatchlistFlavorAuth(*('wlf_auth_%s.xml' % flavor, control.ADDON_PATH),
+                                            flavor=flavor).doModal()
+
+    if auth:
+        return WatchlistFlavor.login_request(flavor)
+    else:
+        return
+
+
+@route('watchlist_login/*')
+def WL_LOGIN(payload, params):
+    if params:
+        return get_auth_dialog(payload)
+
+    return WatchlistFlavor.login_request(payload)
+
+
+@route('watchlist_logout/*')
+def WL_LOGOUT(payload, params):
+    return WatchlistFlavor.logout_request(payload)
+
+
+@route('watchlist/*')
+def WATCHLIST(payload, params):
+    # return control.draw_items(WatchlistFlavor.watchlist_request(payload), contentType=control.getSetting("contenttype.menu"))
+    return control.draw_items(WatchlistFlavor.watchlist_request(payload), contentType="addons")
+
+
+@route('watchlist_status_type/*')
+def WATCHLIST_STATUS_TYPE(payload, params):
+    flavor, status = payload.rsplit("/")
+    return control.draw_items(WatchlistFlavor.watchlist_status_request(flavor, status, params))
+
+
+@route('watchlist_status_type_pages/*')
+def WATCHLIST_STATUS_TYPE_PAGES(payload, params):
+    flavor, status, offset, page = payload.rsplit("/")
+    return control.draw_items(WatchlistFlavor.watchlist_status_request_pages(flavor, status, params, offset, int(page)))
+
+
+@route('watchlist_query/*')
+def WATCHLIST_QUERY(payload, params):
+    anilist_id, mal_id, eps_watched = payload.rsplit("/")
+    show_meta = database.get_show(anilist_id)
+
+    if not show_meta:
+        from resources.lib.AniListBrowser import AniListBrowser
+        show_meta = AniListBrowser().get_anilist(anilist_id)
+
+    kodi_meta = ast.literal_eval(show_meta['kodi_meta'])
+    kodi_meta['eps_watched'] = eps_watched
+    database.update_kodi_meta(anilist_id, kodi_meta)
+
+    anime_general, content_type = _BROWSER.get_anime_init(anilist_id)
+    return control.draw_items(anime_general, content_type)
+
+
+@route('watchlist_to_ep/*')
+def WATCHLIST_TO_EP(payload, params):
+    mal_id, kitsu_id, eps_watched = payload.rsplit("/")
+
+    if not mal_id:
+        return []
+
+    show_meta = database.get_show_mal(mal_id)
+
+    if not show_meta:
+        show_meta = get_anilist_res(mal_id)
+
+    anilist_id = show_meta['anilist_id']
+    kodi_meta = ast.literal_eval(show_meta['kodi_meta'])
+    kodi_meta['eps_watched'] = eps_watched
+    database.update_kodi_meta(anilist_id, kodi_meta)
+
+    if kitsu_id:
+        if not show_meta['kitsu_id']:
+            database.add_mapping_id(anilist_id, 'kitsu_id', kitsu_id)
+
+    anime_general, content_type = _BROWSER.get_anime_init(anilist_id)
+    return control.draw_items(anime_general, content_type)
+
+
+@route('watchlist_to_movie/*')
+def WATCHLIST_TO_MOVIE(payload, params):
+    if params:
+        anilist_id = params['anilist_id']
+        show_meta = database.get_show(anilist_id)
+
+        if not show_meta:
+            from resources.lib.AniListBrowser import AniListBrowser
+            show_meta = AniListBrowser().get_anilist(anilist_id)
+    else:
+        mal_id = payload
+        show_meta = database.get_show_mal(mal_id)
+
+        if not show_meta:
+            show_meta = get_anilist_res(mal_id)
+
+        anilist_id = show_meta['anilist_id']
+
+    sources = _BROWSER.get_sources(anilist_id, '1', None, 'movie')
+    _mock_args = {'anilist_id': anilist_id}
+    from resources.lib.windows.source_select import SourceSelect
+
+    link = SourceSelect(*('source_select.xml', control.ADDON_PATH),
+                        actionArgs=_mock_args, sources=sources).doModal()
+
+    from resources.lib.ui import player
+
+    player.play_source(link)
+
+
+def watchlist_update(anilist_id, episode):
+    flavor = WatchlistFlavor.get_update_flavor()
+    if not flavor:
+        return
+
+    return WatchlistFlavor.watchlist_update_request(anilist_id, episode)
+
+
+def add_watchlist(items):
+    flavors = WatchlistFlavor.get_enabled_watchlists()
+    if not flavors:
+        return
+
+    for flavor in flavors:
+        items.insert(0, (
+            "%s's %s" % (flavor.username, flavor.title),
+            "watchlist/%s" % flavor.flavor_name,
+            flavor.image,
+        ))

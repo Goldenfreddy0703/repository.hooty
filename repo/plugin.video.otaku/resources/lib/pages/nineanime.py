e113e1f9a4e96324e04dc44c0baf792062a97230
Goldenfreddy0703 <jenkins30661@gmail.com>
2023-12-25 01:15:11 -0500

    Rawr

--- repo/plugin.video.otaku/resources/lib/pages/nineanime.py
+++ repo/plugin.video.otaku/resources/lib/pages/nineanime.py
@@ -1,77 +1,168 @@
-import itertools
+import base64
+import codecs
+import json
 import pickle
-from functools import partial
-from six.moves import urllib_parse
+import re
+import six
 
+from bs4 import BeautifulSoup, SoupStrainer
+from Cryptodome.Cipher import ARC4
+from six.moves import urllib_parse
 from resources.lib.ui import control, database
 from resources.lib.ui.BrowserBase import BrowserBase
-from resources.lib.indexers import anify
+from resources.lib.indexers.malsync import MALSYNC
 
 
 class sources(BrowserBase):
+    _BASE_URL = 'https://aniwave.to/'
+    EKEY = 'ysJhV6U27FVIjjuk'
+    DKEY = 'hlPeNwkncH0fq9so'
+    CHAR_SUBST_OFFSETS = (-3, 3, -4, 2, -2, 5, 4, 5)
+
     def get_sources(self, anilist_id, episode, get_backup):
-        all_results = []
         show = database.get_show(anilist_id)
         kodi_meta = pickle.loads(show.get('kodi_meta'))
-        title = kodi_meta.get('ename') or kodi_meta.get('name')
+        title = kodi_meta.get('name')
         title = self._clean_title(title)
-        title = '{0} Ep-{1}'.format(title, episode)
+
+        all_results = []
         srcs = ['sub', 'dub']
         if control.getSetting('general.source') == 'Sub':
             srcs.remove('dub')
         elif control.getSetting('general.source') == 'Dub':
             srcs.remove('sub')
 
-        for x in srcs:
+        items = MALSYNC().get_slugs(anilist_id=anilist_id, site='9anime')
+        if not items:
+            headers = {'Referer': self._BASE_URL}
+            params = {'keyword': title}
             r = database.get(
-                anify.ANIFYAPI().get_sources,
+                self._get_request,
                 8,
-                anilist_id,
-                episode,
-                '9anime',
-                x
+                self._BASE_URL + 'ajax/anime/search',
+                data=params,
+                headers=headers,
+                XHR=True
             )
-            if r and r.get('sources'):
-                srcs = r.get('sources')
-                for i in range(len(srcs)):
-                    srcs[i].update({'type': x.upper()})
-                referer = r.get('headers', {}).get('Referer', '')
-                if referer:
-                    referer = urllib_parse.urljoin(referer, '/')
-                mapfunc = partial(self._process_ap, title=title, referer=referer)
-                results = list(map(mapfunc, srcs))
-                results = list(itertools.chain(*results))
-                all_results += results
+            if not r and ':' in title:
+                title = title.split(':')[0]
+                params.update({'keyword': title})
+                r = database.get(
+                    self._get_request,
+                    8,
+                    self._BASE_URL + 'ajax/anime/search',
+                    data=params,
+                    headers=headers,
+                    XHR=True
+                )
+            if not r:
+                return all_results
+
+            r = BeautifulSoup(json.loads(r).get('html'), "html.parser")
+            sitems = r.find_all('a', {'class': 'item'})
+            if sitems:
+                if title[-1].isdigit():
+                    items = [urllib_parse.urljoin(self._BASE_URL, x.get('href'))
+                             for x in sitems
+                             if title.lower() in x.find('div', {'class': 'name'}).get('data-jp').lower()]
+                else:
+                    items = [urllib_parse.urljoin(self._BASE_URL, x.get('href'))
+                             for x in sitems
+                             if (title.lower() + '  ') in (x.find('div', {'class': 'name'}).get('data-jp').lower() + '  ')]
+
+        if items:
+            slug = items[0]
+            all_results = self._process_aw(slug, title=title, episode=episode, langs=srcs)
 
         return all_results
 
-    def _process_ap(self, item, title='', referer=''):
+    def _process_aw(self, slug, title, episode, langs):
         sources = []
-        slink = item.get('url') + '|Referer={0}&User-Agent=iPad'.format(referer)
-        qual = item.get('quality')
-        if qual.endswith('p'):
-            qual = int(qual[:-1])
-            if qual < 577:
-                quality = 'NA'
-            elif qual < 721:
-                quality = '720p'
-            elif qual < 1081:
-                quality = '1080p'
-            else:
-                quality = '4K'
-        else:
-            quality = 'EQ'
+        headers = {'Referer': self._BASE_URL}
+        r = database.get(
+            self._get_request, 8,
+            slug, headers=headers
+        )
+        sid = re.search(r'id="watch-main.+?data-id="([^"]+)', r)
+        if not sid:
+            return sources
 
-        source = {
-            'release_title': title,
-            'hash': slink,
-            'type': 'direct',
-            'quality': quality,
-            'debrid_provider': '',
-            'provider': 'Aniwave',
-            'size': 'NA',
-            'info': [item.get('type')],
-            'lang': 0 if item.get('type') == 'SUB' else 2
-        }
-        sources.append(source)
+        sid = sid.group(1)
+        vrf = self.generate_vrf(sid)
+        params = {'vrf': vrf}
+        r = database.get(
+            self._get_request, 8,
+            '{0}ajax/episode/list/{1}'.format(self._BASE_URL, sid),
+            headers=headers, data=params,
+            XHR=True
+        )
+        res = json.loads(r).get('result')
+
+        elink = SoupStrainer('div', {'class': re.compile('^episodes')})
+        ediv = BeautifulSoup(res, "html.parser", parse_only=elink)
+        items = ediv.find_all('a')
+        e_id = [x.get('data-ids') for x in items if x.get('data-num') == episode]
+        if e_id:
+            e_id = e_id[0]
+            vrf = self.generate_vrf(e_id)
+            params = {'vrf': vrf}
+            r = database.get(
+                self._get_request, 8,
+                '{0}ajax/server/list/{1}'.format(self._BASE_URL, e_id),
+                data=params, headers=headers,
+                XHR=True
+            )
+            eres = json.loads(r).get('result')
+            for lang in langs:
+                elink = SoupStrainer('div', {'data-type': lang})
+                sdiv = BeautifulSoup(eres, "html.parser", parse_only=elink)
+                srcs = sdiv.find_all('li')
+                for src in srcs:
+                    edata_id = src.get('data-link-id')
+                    edata_name = src.text
+                    vrf = self.generate_vrf(edata_id)
+                    params = {'vrf': vrf}
+                    r = self._get_request(
+                        '{0}ajax/server/{1}'.format(self._BASE_URL, edata_id),
+                        data=params,
+                        headers=headers,
+                        XHR=True
+                    )
+                    resp = json.loads(r).get('result')
+                    slink = self.decrypt_vrf(resp.get('url'))
+
+                    source = {
+                        'release_title': '{0} - Ep {1}'.format(title, episode),
+                        'hash': slink,
+                        'type': 'embed',
+                        'quality': 'EQ',
+                        'debrid_provider': '',
+                        'provider': 'aniwave',
+                        'size': 'NA',
+                        'info': ['DUB' if lang == 'dub' else 'SUB', edata_name],
+                        'lang': 2 if lang == 'dub' else 0,
+                    }
+                    sources.append(source)
         return sources
+
+    @staticmethod
+    def vrf_shift(t, offset=CHAR_SUBST_OFFSETS):
+        o = ''
+        for s in range(len(t)):
+            o += chr(ord(t[s]) + offset[s % 8])
+        return o
+
+    def generate_vrf(self, content_id, key=EKEY):
+        vrf = ARC4.new(six.ensure_binary(key)).encrypt(six.b(urllib_parse.quote(content_id)))
+        vrf = six.ensure_str(base64.urlsafe_b64encode(vrf))
+        vrf = six.ensure_str(base64.b64encode(six.b(vrf)))
+        vrf = self.vrf_shift(vrf)
+        vrf = six.ensure_str(base64.b64encode(six.b(vrf)))
+        vrf = codecs.encode(vrf, 'rot_13')
+        return vrf.replace('/', '_').replace('+', '-')
+
+    @staticmethod
+    def decrypt_vrf(text, key=DKEY):
+        data = ARC4.new(six.ensure_binary(key)).decrypt(base64.urlsafe_b64decode(six.b(text)))
+        data = urllib_parse.unquote(six.ensure_str(data))
+        return data
--- repo/plugin.video.otaku/resources/lib/ui/embed_extractor.py
+++ repo/plugin.video.otaku/resources/lib/ui/embed_extractor.py
@@ -4,9 +4,9 @@ import random
 import re
 import string
 import time
-
 import six
-from resources.lib.ui import client, control, jsunpack
+
+from resources.lib.ui import client, control, database, jsunpack
 from resources.lib.ui.pyaes import AESModeOfOperationCBC, Decrypter, Encrypter
 from six.moves import urllib_error, urllib_parse
 
@@ -124,6 +124,62 @@ def __extract_mp4upload(url, page_content, referer=None):
     if r:
         return r.group(1) + __append_headers(headers)
     return
+
+
+def __extract_vidplay(url, page_content, referer=None):
+    def dex(key, data, encode=True):
+        x = 0
+        ct = ''
+        y = list(range(256))
+        for r in range(256):
+            u = key[r % len(key)]
+            x = (x + y[r] + (u if isinstance(u, int) else ord(u))) % 256
+            t = y[r]
+            y[r] = y[x]
+            y[x] = t
+
+        s = 0
+        x = 0
+        for r in range(len(data)):
+            s = (s + 1) % 256
+            x = (x + y[s]) % 256
+            t = y[s]
+            y[s] = y[x]
+            y[x] = t
+            ct += chr((data[r] if isinstance(data[r], int) else ord(data[r])) ^ y[(y[s] + y[x]) % 256])
+
+        if encode:
+            ct = six.ensure_str(base64.b64encode(six.b(ct))).replace('/', '_').replace('+', '-')
+
+        return ct
+
+    def encode_id(id_):
+        keys = database.get(
+            client.request, 0.2,
+            'https://raw.githubusercontent.com/Claudemirovsky/worstsource-keys/keys/keys.json'
+        )
+        k1, k2 = json.loads(keys)
+        v = dex(k1, id_, False)
+        v = dex(k2, v)
+        return v
+
+    headers = {'User-Agent': _EDGE_UA}
+    turl = urllib_parse.urljoin(url, '/futoken')
+    r = six.ensure_str(client.request(turl, referer=url, headers=headers))
+    k = re.search(r"var\s*k='([^']+)", r)
+    if k:
+        v = encode_id(url.split('?')[0].split('/')[-1])
+        k = k.group(1)
+        a = [k]
+        for i in range(len(v)):
+            a.append(str(ord(k[i % len(k)]) + ord(v[i])))
+        murl = urllib_parse.urljoin(url, '/mediainfo/' + ','.join(a) + '?' + url.split('?')[-1])
+        s = json.loads(client.request(murl, referer=url, XHR=True, headers=headers))
+        if isinstance(s.get('result'), dict):
+            uri = s.get('result').get('sources')[0].get('file')
+            rurl = urllib_parse.urljoin(murl, '/')
+            uri += '|Referer={0}&Origin={1}&User-Agent=iPad'.format(rurl, rurl[:-1])
+            return uri
 
 
 def __extract_kwik(url, page_content, referer=None):
@@ -312,6 +368,10 @@ def __relative_url(original_url, new_url):
 __register_extractor(["https://www.mp4upload.com/",
                       "https://mp4upload.com/"],
                      __extract_mp4upload)
+
+__register_extractor(["https://vidplay.online/",
+                      "https://mcloud.bz/"],
+                     __extract_vidplay)
 
 __register_extractor(["https://kwik.cx/"],
                      __extract_kwik)

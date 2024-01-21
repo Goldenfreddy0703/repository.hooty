a4f1e7ada86aaea4461f749328f5c5e9697a7f58
Goldenfreddy0703 <jenkins30661@gmail.com>
2024-01-20 21:21:37 -0500

    Rawr

--- repo/plugin.video.otaku/resources/lib/pages/aniplay.py
+++ repo/plugin.video.otaku/resources/lib/pages/aniplay.py
@@ -1,20 +1,19 @@
 import json
 import pickle
-import re
 
-from resources.lib.ui import database, source_utils
+from resources.lib.ui import database
 from resources.lib.ui.BrowserBase import BrowserBase
 
 
 class sources(BrowserBase):
-    _BASE_URL = 'https://aniplay.co/' 
+    _BASE_URL = 'https://aniplay.co/'
 
     def get_sources(self, anilist_id, episode, get_backup):
         show = database.get_show(anilist_id)
         kodi_meta = pickle.loads(show.get('kodi_meta'))
         title = kodi_meta.get('name')
         title = self._clean_title(title)
-          
+
         params = {'query': title}
         res = database.get(
             self._get_request,
@@ -25,8 +24,14 @@ class sources(BrowserBase):
         items = json.loads(res)
         all_results = []
         if items:
- 
-            ids = [item.get("id") for item in items if any(f"https://anilist.co/anime/{anilist_id}" in website["url"] for website in item.get("listWebsites", []))]
+
+            ids = [
+                item.get("id") for item in items
+                if any(
+                    "https://anilist.co/anime/{}".format(anilist_id) in website["url"]
+                    for website in item.get("listWebsites", [])
+                )
+            ]
 
             all_results = self._process_al(ids, title=title, episode=episode)
 
@@ -35,11 +40,11 @@ class sources(BrowserBase):
     def _process_al(self, ids, title, episode):
         sources = []
 
-        for id in ids: 
+        for id in ids:
             url = '{0}api/anime/{1}'.format(
                 self._BASE_URL, id
-
             )
+
             res = database.get(
                 self._get_request,
                 8,
@@ -54,9 +59,12 @@ class sources(BrowserBase):
 
             else:
                 items = json.loads(res).get('seasons')
-                season_id = next(season["id"] for season in sorted(items, key=lambda x: x["episodeStart"]) if int(episode) >= int(season["episodeStart"]))
+                season_id = next(
+                    season["id"] for season in sorted(items, key=lambda x: -x["episodeStart"])
+                    if int(episode) >= int(season["episodeStart"])
+                )
                 url = '{0}api/anime/{1}/season/{2}'.format(
-                self._BASE_URL, id, season_id
+                    self._BASE_URL, id, season_id
                 )
                 res = database.get(
                     self._get_request,
@@ -65,27 +73,28 @@ class sources(BrowserBase):
                 )
                 items = json.loads(res)
                 e_id = [x["id"] for x in items if str(x.get('episodeNumber')) == str(episode)]
+
             if e_id:
-                    url = '{0}api/episode/{1}'.format(
+                url = '{0}api/episode/{1}'.format(
                     self._BASE_URL, e_id[0]
-                    )
-                    res = database.get(
-                        self._get_request,
-                        8,
-                        url
-                    )
-                    slink = json.loads(res).get('videoUrl')
-                    source = {
-                                    'release_title': '{0} - Ep {1}'.format(title, episode),
-                                    'hash': slink,
-                                    'type': 'direct',
-                                    'quality': 'EQ',
-                                    'debrid_provider': '',
-                                    'provider': 'aniplay',
-                                    'size': 'NA',
-                                    'info': [lang],
-                                    'lang': 2 if lang == 'DUB' else 0
-                                }
-                    sources.append(source)
+                )
+                res = database.get(
+                    self._get_request,
+                    8,
+                    url
+                )
+                slink = json.loads(res).get('videoUrl')
+                source = {
+                    'release_title': '{0} - Ep {1}'.format(title, episode),
+                    'hash': slink,
+                    'type': 'direct',
+                    'quality': 'EQ',
+                    'debrid_provider': '',
+                    'provider': 'aniplay',
+                    'size': 'NA',
+                    'info': [lang],
+                    'lang': 2 if lang == 'DUB' else 0
+                }
+                sources.append(source)
 
         return sources

253918b5f9640a5c5f8699edf5ed401f0eb5813c
Goldenfreddy0703 <jenkins30661@gmail.com>
2024-04-29 16:14:37 -0400

    Rawr

--- repo/plugin.video.otaku/service.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from resources.lib.ui import maintenance
-from resources.lib.ui import database_sync
-from resources.lib.ui import monitor
-
-database_sync.AnilistSyncDatabase()
-maintenance.run_maintenance()
-monitor.SettingsMonitor()

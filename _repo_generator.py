af48b9ebb0160bf1ac9bd3ed83a09652487ad193
Goldenfreddy0703 <jenkins30661@gmail.com>
2024-07-09 01:36:49 -0400

    Rawr

--- _repo_generator.py
+++ _repo_generator.py
@@ -13,7 +13,7 @@ import zipfile
 from xml.etree import ElementTree
 
 SCRIPT_VERSION = 5
-KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "repo"]
+KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "omega", "repo"]
 IGNORE = [
     ".git",
     ".github",

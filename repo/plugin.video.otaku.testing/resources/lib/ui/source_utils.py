ed051ea9e94ae2e1f263b2d97567ee35bb0923a2
Goldenfreddy0703 <jenkins30661@gmail.com>
2025-04-16 05:30:45 -0400

    Rawr

--- repo/plugin.video.otaku.testing/resources/lib/ui/source_utils.py
+++ repo/plugin.video.otaku.testing/resources/lib/ui/source_utils.py
@@ -251,6 +251,7 @@ def get_fuzzy_match(query, filenames):
 
 
 def get_best_match(dict_key, dictionary_list, episode, pack_select=False):
+    control.setSetting('best_match', 'true')
     regex = get_cache_check_reg(episode)
     files = []
     for i in dictionary_list:
@@ -264,6 +265,7 @@ def get_best_match(dict_key, dictionary_list, episode, pack_select=False):
     else:
         files = [i for i in files if len(i['regex_matches']) > 0]
         if len(files) == 0:
+            control.setSetting('best_match', 'false')
             return {}
         files = sorted(files, key=lambda x: len(' '.join(list(x['regex_matches'][0]))), reverse=True)
         if len(files) != 1:

cd166baaffd7965dcb106f58aded583c9432f964
Goldenfreddy0703 <jenkins30661@gmail.com>
2025-07-30 02:11:33 -0400

    Rawr

--- repo/plugin.video.otaku.testing/resources/lib/pages/__init__.py
+++ repo/plugin.video.otaku.testing/resources/lib/pages/__init__.py
@@ -8,7 +8,7 @@ from resources.lib.windows import sort_select
 
 
 def getSourcesHelper(actionArgs):
-    if control.getInt('general.dialog') in (5, 6):
+    if control.getSetting('general.dialog') in (5, 6):
         sources_window = Sources('get_sources_alt.xml', control.ADDON_PATH, actionArgs=actionArgs)
     else:
         sources_window = Sources('get_sources.xml', control.ADDON_PATH, actionArgs=actionArgs)
